80d5687417bd5baf42cf45ca2d575d0e10a03b79
Yong San Chia <yongsan@Yongs-MacBook-Pro.local>
2022-10-16 21:06:46 +0800

    feat: hello world

---
+++ src/github.com/letsmakelove/user-service/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		return
+	}
+}
